79a4c100ff2ae59bcdaaee5ed834935caac4b6c8
jakemraz <jakemraz100@gmail.com>
2019-12-11 14:28:53 +0900

    fix config.py

--- lambda_fn/config.py
+++ lambda_fn/config.py
@@ -1,9 +1,5 @@
 # config.py
 
 PINPOINT_CONFIG = {
-<<<<<<< HEAD
     'application_id': '<PUT_PINPOINT_APPLICATION_ID_HERE>'
-=======
-    'application_id': '6dd52750e1b54397ad258c8827ee7a00'
->>>>>>> 74c2c774c1a7686412934d5dba6420d8082e50b6
 }
